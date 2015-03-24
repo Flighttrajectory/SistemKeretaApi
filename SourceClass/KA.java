3da5b80a726d0c78608ff8412ce882af4731faf5
Flighttrajectory <robindra22@gmail.com>
2015-03-24 18:14:50 +0700

    Update KA.java

--- SourceClass/KA.java
+++ SourceClass/KA.java
@@ -10,5 +10,49 @@ package SourceClass;
  * @author heavydude
  */
 public class KA {
+    private String nama ;
+    private int jumlahGerbong;
+    private Gerbong [] gerbong = new Gerbong [12];
+    private RuteKA rute;
+    
+    public KA (String nama){
+        this.nama = nama;
+    }
+    public KA (String nama , int jumlahGerbong){
+        this.nama=nama;
+        this.jumlahGerbong=jumlahGerbong;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setJumlahGerbong(int jumlahGerbong) {
+        this.jumlahGerbong = jumlahGerbong;
+    }
+    public void setGerbong(Gerbong[] gerbong) {
+        this.gerbong = gerbong;
+    }
+    public void setRute(RuteKA rute) {
+        this.rute = rute;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int getJumlahGerbong() {
+        return jumlahGerbong;
+    }
+    public Gerbong[] getGerbong() {
+        return gerbong;
+    }
+    public RuteKA getRute() {
+        return rute;
+    }
+    public void buatGerbong(Gerbong gerbong){
+        if (jumlahGerbong >12) {
+            System.out.println("Kereta sudah penuh");
+        }else{
+           this.gerbong[jumlahGerbong]=gerbong;
+           jumlahGerbong++;
+        }
+    }
     
 }
